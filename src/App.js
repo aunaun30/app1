2340dbe1bba28368bb0991ec7fe5674d128d7eaa
aunaun30 <152370449+aunaun30@users.noreply.github.com>
2024-01-19 21:49:11 +0700

    add state-func
    add state-func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import RefsArray from './refs-array'
+import StateFunc from './state-func'
 
 
 function App(){
   return (
-    <RefsArray/>
+    <StateFunc/>
   )
 }
 
