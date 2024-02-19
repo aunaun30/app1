d79611ec9b0bf6c9b1144642331f4c7a50ad3749
aunaun30 <s6503052412153@email.kmutnb.ac.th>
2024-02-19 10:49:42 +0700

    form-text part_1
    form-text part_1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import FormSearch from './form-search';
+import FormText from './form-text';
 export function App(){
-    return <FormSearch/>
+    return <FormText/>
 }
 export default App
